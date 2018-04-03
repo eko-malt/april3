30b1f56905c09656fb667387da57180687f7484c
kostyaha <kostya.kokosko@gmail.com>
2018-04-03 11:20:58 +0300

    fix doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -3,9 +3,5 @@ class Doctor < ApplicationRecord
   validates_presence_of :first_name, :phone, :city, :spec, message: I18n.t('form.cant_be_blank')
   validate :check_count
 
-  def check_count
-    if Doctor.where(club: club).count > 70
-      errors.add(:club, I18n.t('doctor.club_limit'))
-    end
-  end
+  
 end
