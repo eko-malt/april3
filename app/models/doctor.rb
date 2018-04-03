6c2dafb6ff6cc8903acb788c0755a2ba4894b6b9
kostyaha <kostya.kokosko@gmail.com>
2018-04-03 11:23:19 +0300

    fix doctor model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,5 @@
 class Doctor < ApplicationRecord
   validates_uniqueness_of :phone, message: I18n.t('form.phone_already_taken')
   validates_presence_of :first_name, :phone, :city, :spec, message: I18n.t('form.cant_be_blank')
-  validate :check_count
 
-  
 end
